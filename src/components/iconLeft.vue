bc002fc22e02e715f348e3f2751823e5514fa803
lioncoder8 <lioncoder8@gmail.com>
2023-04-21 19:35:59 -0700

    update-data

--- src/components/iconLeft.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="12.993" height="12.993" viewBox="0 0 12.993 12.993">
-      <g id="グループ_4495" data-name="グループ 4495" transform="translate(602.497 -259.755) rotate(90)">
-        <path id="Path" d="M6.5,0A6.5,6.5,0,1,0,11.09,1.9,6.5,6.5,0,0,0,6.5,0Z" transform="translate(272.749 589.503) rotate(90)" fill="#2e3330"/>
-        <path id="多角形_1" data-name="多角形 1" d="M3.227.942a1,1,0,0,1,1.546,0l1.886,2.3a1,1,0,0,1-.773,1.634H2.114A1,1,0,0,1,1.341,3.24Z" transform="translate(270.252 599.299) rotate(-180)" fill="#fff"/>
-      </g>
-    </svg>
-</template>
-  
