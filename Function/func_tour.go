bd2003d4566f7b1387ab86c60bafb2c9a32e8aba
lioncoder8 <lioncoder8@gmail.com>
2023-04-26 02:30:06 +0900

    update

--- Function/func_tour.go
+++ Function/func_tour.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
-	}
-	return fmt.Sprint(math.Sqrt(x))
-}
-
 func main() {
-	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Print("Go runs on\n")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s. \n", os)
+
+	}
 }
